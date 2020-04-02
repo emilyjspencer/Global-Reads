bc80cb3354277194122dcc2061423a02c1988fcb
Emily <emilyspencer89@hotmail.co.uk>
2020-04-02 15:09:56 +0100

    Adds user_id column to reviews table to make the link between users and reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_140025) do
+ActiveRecord::Schema.define(version: 2020_04_02_140829) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.text "review"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
