ca5624e6445fe684792ef3865976c0ebe8712514
Emily <emilyspencer89@hotmail.co.uk>
2020-04-02 12:12:42 +0100

    Adds create action to ReviewsController and a private method review_requirements

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,22 @@ class ReviewsController < ApplicationController
     def new
         @review = Review.new
     end
+
+    def create
+        @review = Review.new(review_requirements)
+        @review.save
+        redirect_to reviews_path(@review)
+    end
+
+    
+
+
+
+
+
+    private
+
+    def review_requirements
+      params.require(:review).permit(:title, :review)
+    end
 end 
