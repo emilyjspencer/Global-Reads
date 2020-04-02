4f030f2097999e848a4cfa0e15c81b6d328d5100
Emily <emilyspencer89@hotmail.co.uk>
2020-04-02 15:14:05 +0100

    Makes an association between reviews and users

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
+    belongs_to :user
+    validates :user_id, presence: true
     validates :title, presence: true, length: { minimum: 4, maximum: 40 }
     validates :review, presence: true, length: { minimum: 20, maximum: 800 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+ has_many :reviews
  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 20 }
  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
  validates :email, presence: true, length: { maximum: 30 }, uniqueness: { case_sensitive: false }, 
