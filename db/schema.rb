78d6713dd941d84dddf1e6de63c9015c19e187ff
Emily <emilyspencer89@hotmail.co.uk>
2020-04-02 15:02:04 +0100

    Creates a users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_105637) do
+ActiveRecord::Schema.define(version: 2020_04_02_140025) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.text "review"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
