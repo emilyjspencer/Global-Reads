c69b5eb57e0fa8ef0ef92dfd670a6393697ab418
Emily <emilyspencer89@hotmail.co.uk>
2020-04-02 15:07:13 +0100

    Adds user credentials and validations. A user must have a username and a password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+ validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 20 }
+ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+ validates :email, presence: true, length: { maximum: 30 }, uniqueness: { case_sensitive: false }, 
+ format: { with: VALID_EMAIL_REGEX }
+ before_save { self.email = email.downcase }
 end 
