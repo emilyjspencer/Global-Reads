91904cd8c976247d7d79854944e0310a6b39b2be
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-04-22 17:55:59 +0100

    Adds a destroy action to the Users Controller, along with a 'before' filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,19 @@
 class UsersController < ApplicationController
 
-  before_action :user_credentials, only: [:show, :edit, :update]
+  before_action :user_credentials, only: [:show, :edit, :update, :destroy]
+  before_action :require_admin, only [:destroy]
+  before_action :require_same_user, only: [: destroy, :edit, :update]
 
-    def new
+    def new 
       @user = User.new
     end
 
     def create
         @user = User.new(user_credentials)
        if @user.save
+         session[:user_id] = @user.id
          flash[:success] = "Welcome to the Global Reads #{@user.username}"
-         redirect_to reviews_path
+         redirect_to user_path(@user)
        else
          render 'new'
        end
@@ -36,16 +39,36 @@ class UsersController < ApplicationController
       @users = User.paginate(page: params[:page], per_page: 5)
      end
 
+     def destroy
+      @user.destroy
+      flash[:danger] = "User and all reviews created by user have been deleted"
+      redirect_to users_path
+    end 
+
 
     private
 
     def user_credentials
-      params.require(:user).permit(:username, :email, :password)
-      
+
+      params.require(:user).permit(:username, :email, :password) 
     end 
 
     def find_by_id
       @user = User.find(params[:id])
+    end
+
+    def require_admin
+      if logged_in? and !current_user.admin?
+        flash[:danger] = "Only administrators can perform that action"
+        redirect_to root_path
+      end 
+    end 
+
+    def require_same_user
+      if current_user != @user and !current_user.admin? 
+        flash[:danger] = "You can only edit your own account"
+        redirect_to root_path
+      end
     end
 
 end
