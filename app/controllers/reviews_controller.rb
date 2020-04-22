4093052242b20f5031fc8955cd65174778de7df7
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-04-22 03:10:47 +0100

    Adds before actions and private method require_same_user. Users now cannot delete or edit reviews that don't belong to them

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,8 @@
 class ReviewsController < ApplicationController
 
     before_action :find_by_id, only: [:edit, :update, :show, :destroy]
+    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_user, except: [:index, :show]
 
 
     def new
@@ -9,7 +11,8 @@ class ReviewsController < ApplicationController
 
     def create
         @review = Review.new(review_requirements)
-        @review.user = User.first
+       # @review.user = User.first
+       @review.user = current_user 
         if @review.save
           flash[:success] = "Your review was successfully created"
           redirect_to reviews_path(@review)
@@ -40,7 +43,7 @@ class ReviewsController < ApplicationController
 
       def destroy
         @review.destroy
-        flash[:success] = "Your review deleted"
+        flash[:success] = "Your review was deleted"
         redirect_to reviews_path
        end
 
@@ -59,4 +62,11 @@ class ReviewsController < ApplicationController
     def review_requirements
       params.require(:review).permit(:title, :review)
     end
+
+    def require_same_user
+      if current_user != @review.user
+        flash[:danger] = "You can only edit or delete your own review"
+        redirect_to root_path
+      end
+    end 
 end 
