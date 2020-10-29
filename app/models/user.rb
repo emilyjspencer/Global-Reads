f58a81ae25598f13d3c4c52818b56b907b75e903
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-10-29 20:54:18 +0000

    Updates About page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   before_save { self.email = email.downcase }
-
- validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 20 }
- VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
- validates :email, presence: true, length: { maximum: 30 }, uniqueness: { case_sensitive: false }, 
- format: { with: VALID_EMAIL_REGEX }
-
- has_secure_password
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 30 }, uniqueness: { case_sensitive: false }, 
+  format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end 
